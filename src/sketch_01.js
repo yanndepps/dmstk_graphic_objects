fe60ade4f24eaf5555fabf6f5288d2338a2f032a
yanndepps <yann.deperrot@protonmail.com>
2022-11-26 13:24:14 +0100

    pattern grids 01

--- src/sketch_01.js
+++ src/sketch_01.js
@@ -1,17 +1,35 @@
+/*
+ * Intro to Creative Coding -> Create Graphic Objects
+ * pattern grids 01
+ */
+
 const canvasSketch = require('canvas-sketch');
 const p5 = require('p5');
 new p5();
 
 const settings = {
   p5: true,
-  dimensions: [ 500, 500 ],
+  dimensions: [500, 500],
   animate: true,
   context: '2d'
 };
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-	background(33);
+let nb = 20;
+let dim;
+let f = 0.9;
+let margin = 20;
+
+const sketch = ({ width, height }) => {
+  dim = (width - 2 * margin) / nb;
+  return () => {
+    background(0);
+    noStroke();
+    fill(255);
+    for (let j = 0; j < nb; j++) {
+      for (let i = 0; i < nb; i++) {
+        circle(margin + dim / 2 + i * dim, margin + dim / 2 + j * dim, f * dim);
+      }
+    }
   };
 };
 
